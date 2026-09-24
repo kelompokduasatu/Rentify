cffbf5cdb50e8d22b74f4f7cec4c9b8e1c6c9f11
RakanSewon <RakanSewon>
2026-09-24 22:00:28 +0700

    feat: initiate pages directory

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is the About Page</h1>
+    </div>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is the Login Page</h1>
+    </div>
+  );
+}
